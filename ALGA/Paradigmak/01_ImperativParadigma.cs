5985917c1e36b6386470bf315aad19e7cd61b2db
hallgato <hallgato@C202-32>
2025-09-08 15:16:11 +0200

    base

--- ALGA/Paradigmak/01_ImperativParadigma.cs
+++ ALGA/Paradigmak/01_ImperativParadigma.cs
@@ -5,5 +5,52 @@ using System.Collections.Generic;
 namespace OE.ALGA.Paradigmak
 {
     // 1. heti labor feladat - Tesztek: 01_ImperativParadigmaTesztek.cs
+    public interface IVegrehajthato
+    {
+        public void Vegrehajtas();
+    }
+
+    public class TaroloMegteltKivetel : Exception { }
+
+    public class FeladatTarolo<T> : IEnumerable<T> where T : IVegrehajthato
+    {
+        T[] tarolo;
+        int n;
+
+        public FeladatTarolo(int size)
+        {
+            tarolo = new T[size];
+            n = 0;
+        }
+
+        public void Felvesz(T elem)
+        {
+            throw new TaroloMegteltKivetel();
+            //todo
+        }
+
+        public void MindentVegrehajt()
+        {
+            //todo
+        }
+    }
+
+    public class FeladatTaroloBejaro<T> : IEnumerable<T>
+    { }
+
+    public interface IFuggo
+    {
+        public bool FuggosegTeljesul { get; }
+    }
+
+    public class FuggoFeladatTarolo<T> : FeladatTarolo<T> where T : IVegrehajthato, IFuggo
+    {
+        public FuggoFeladatTarolo(int size):base(size) 
+        {
+            
+        }
+
+        public bool FuggosegTeljesul => throw new NotImplementedException();
+    }
 }
 

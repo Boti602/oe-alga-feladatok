39b7760994d8133f1357b33eac96ccef82ac6a91
Boti602 <bardoczkybotond@stud.uni-obuda.hu>
2025-09-28 23:53:48 +0200

    base

--- ALGA/Adatszerkezetek/03_TombImplementacio.cs
+++ ALGA/Adatszerkezetek/03_TombImplementacio.cs
@@ -53,9 +53,8 @@ namespace OE.ALGA.Adatszerkezetek
         {
             if (n > 0)
             {
-                T ertek = E[n - 1];
                 n--;
-                return ertek;
+                return E[n];
             }
             else
             {
@@ -98,10 +97,10 @@ namespace OE.ALGA.Adatszerkezetek
 
         public void Sorba(T ertek)
         {
-            if (n < E.Length) 
+            if (n < E.Length)
             {
                 n++;
-                u = (u+1) % E.Length;
+                u = (u + 1) % E.Length;
                 E[u] = ertek;
             }
             else
@@ -112,10 +111,10 @@ namespace OE.ALGA.Adatszerkezetek
 
         public T Sorbol()
         {
-            if( n > 0)
+            if (n > 0)
             {
                 n--;
-                e = (e+1) % E.Length;
+                e = (e + 1) % E.Length;
                 return E[e];
             }
             else
@@ -125,14 +124,17 @@ namespace OE.ALGA.Adatszerkezetek
         }
     }
 
-    public class TombLista<T> : Lista<T>
+    public class TombLista<T> : Lista<T>, IEnumerable<T>
     {
         T[] E;
-        int n = 0;
+        int n;
+
+        public TombLista() : this(4) { }
 
         public TombLista(int meret)
         {
             E = new T[meret];
+            n = 0;
         }
 
         public int Elemszam
@@ -145,32 +147,138 @@ namespace OE.ALGA.Adatszerkezetek
 
         public void Bejar(Action<T> muvelet)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < n; i++)
+            {
+                muvelet(E[i]);
+            }
         }
 
         public void Beszur(int index, T ertek)
         {
-            throw new NotImplementedException();
+            if (index <= n)
+            {
+                if (n == E.Length)
+                {
+                    MeretNovel();
+                }
+
+                n++;
+                for (int i = n-1; i > index; i--)
+                {
+                    E[i] = E[i - 1];
+                }
+                E[index] = ertek;
+            }
+            else
+            {
+                throw new HibasIndexKivetel();
+            }
         }
 
         public void Hozzafuz(T ertek)
         {
-            throw new NotImplementedException();
+            Beszur(n, ertek);
         }
 
         public T Kiolvas(int index)
         {
-            throw new NotImplementedException();
+            if (index <= n - 1)
+            {
+                return E[index];
+            }
+            else
+            {
+                throw new HibasIndexKivetel();
+            }
         }
 
         public void Modosit(int index, T ertek)
         {
-            throw new NotImplementedException();
+            if (index <= n - 1)
+            {
+                E[index] = ertek;
+            }
+            else
+            {
+                throw new HibasIndexKivetel();
+            }
         }
 
         public void Torol(T ertek)
         {
-            throw new NotImplementedException();
+            int db = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (E[i]!.Equals(ertek))
+                {
+                    db++;
+                }
+                else
+                {
+                    E[i - db] = E[i];
+                }
+            }
+            n = n - db;
+        }
+        public void MeretNovel()
+        {
+            T[] E2 = E;
+            E = new T[E.Length * 2];
+            for (int i = 0; i < n; i++)
+            {
+                E[i] = E2[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            TombListaBejaro<T> bejaro = new TombListaBejaro<T>(E, n);
+            return bejaro;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class TombListaBejaro<T> : IEnumerator<T>
+    {
+        T[] E;
+        int n = 0;
+        int aktualisIndex = -1;
+
+        public TombListaBejaro(T[] E, int n)
+        {
+            this.E = E;
+            this.n = n;
+        }
+
+        public T Current => E[aktualisIndex];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            if (aktualisIndex < n - 1)
+            {
+                aktualisIndex++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            aktualisIndex = -1;
         }
     }
 }

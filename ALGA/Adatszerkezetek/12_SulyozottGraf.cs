ba706207225d24ca726fe1220e7a3b033ddfdd14
Boti602 <bardoczkybotond@stud.uni-obuda.hu>
2025-11-29 21:01:33 +0100

    base

--- ALGA/Adatszerkezetek/12_SulyozottGraf.cs
+++ ALGA/Adatszerkezetek/12_SulyozottGraf.cs
@@ -13,6 +13,14 @@ namespace OE.ALGA.Adatszerkezetek
         }
 
         public float Suly { get; }
+
+        public int CompareTo(EgeszGrafEl other)
+        {
+            if (other == null) return 1;
+            if (Honnan != other.Honnan)
+                return Honnan.CompareTo(other.Honnan);
+            return Hova.CompareTo(other.Hova);
+        }
     }
 
     public class CsucsmatrixSulyozottEgeszGraf : SulyozottGraf<int, SulyozottEgeszGrafEl>
@@ -23,6 +31,7 @@ namespace OE.ALGA.Adatszerkezetek
         public CsucsmatrixSulyozottEgeszGraf(int n)
         {
             this.n = n;
+            M = new float[n, n];
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
@@ -43,7 +52,8 @@ namespace OE.ALGA.Adatszerkezetek
                 {
                     for (int j = 0; j < n; j++)
                     {
-                        if ((M[i, j] != float.NaN)) elekSzama++;
+                        if (!float.IsNaN(M[i,j])) 
+                            elekSzama++;
                     }
                 }
                 return elekSzama;
@@ -74,7 +84,7 @@ namespace OE.ALGA.Adatszerkezetek
                     {
                         if ((!float.IsNaN(M[i, j])))
                         {
-                            elek.Beszur(new SulyozottEgeszGrafEl(i, j, float.NaN));
+                            elek.Beszur(new SulyozottEgeszGrafEl(i, j, M[i,j]));
                         }
                     }
                 }
@@ -115,7 +125,7 @@ namespace OE.ALGA.Adatszerkezetek
         public bool VezetEl(int honnan, int hova)
         {
             if(!float.IsNaN(M[honnan, hova]))
-                {
+            {
                 return true;
             }
             else return false;
@@ -126,16 +136,16 @@ namespace OE.ALGA.Adatszerkezetek
     {
         public static Szotar<V, float> Dijkstra<V, E>(SulyozottGraf<V, E> g, V start)
         {
-            Szotar<V, float> P = new HasitoSzotarTulcsordulasiTerulettel<V, float>(g.CsucsokSzama);
-            Szotar<V, V> L = new HasitoSzotarTulcsordulasiTerulettel<V, V>(g.CsucsokSzama);
-            KupacPrioritasosSor<V> S = new KupacPrioritasosSor<V>(g.CsucsokSzama, (ez, ennel) => P.Kiolvas(ez) < P.Kiolvas(ennel));
+            Szotar<V, float> L = new HasitoSzotarTulcsordulasiTerulettel<V, float>(g.CsucsokSzama);
+            Szotar<V, V> P = new HasitoSzotarTulcsordulasiTerulettel<V, V>(g.CsucsokSzama);
+            KupacPrioritasosSor<V> S = new KupacPrioritasosSor<V>(g.CsucsokSzama, (ez, ennel) => L.Kiolvas(ez) < L.Kiolvas(ennel));
 
             g.Csucsok.Bejar(x =>
             {
-                P.Beir(x, float.MaxValue);
+                L.Beir(x, float.MaxValue);
                 S.Sorba(x);
             });
-            P.Beir(start, 0);
+            L.Beir(start, 0);
             S.Frissit(start);
             while (!S.Ures)
             {
@@ -144,14 +154,92 @@ namespace OE.ALGA.Adatszerkezetek
                 {
                    if((L.Kiolvas(u) + g.Suly(u, x)) < L.Kiolvas(x))
                    {
-                        L.Beir(x, L.Kiolvas(u) + g.Suly(u, x);
+                        L.Beir(x, L.Kiolvas(u) + g.Suly(u, x));
                         P.Beir(x, u);
 
                         S.Frissit(x);
                    }
                 });
             }
-            return  ;
+            return L;
+        }
+    }
+
+    public class FeszitofaKereses
+    {
+        public static Szotar<V, V> Prim<V, E>(SulyozottGraf<V, E> g, V start) where V : IComparable<V>
+        {
+            Szotar<V, V> P = new HasitoSzotarTulcsordulasiTerulettel<V, V>(g.CsucsokSzama);
+            Szotar<V, float> K = new HasitoSzotarTulcsordulasiTerulettel<V, float>(g.CsucsokSzama);
+            KupacPrioritasosSor<V> S = new KupacPrioritasosSor<V>(g.CsucsokSzama, (ez, ennel) => K.Kiolvas(ez).CompareTo(K.Kiolvas(ennel)) < 0);
+            Halmaz<V> W = new FaHalmaz<V>();
+
+            g.Csucsok.Bejar(x =>
+            {
+                K.Beir(x, float.MaxValue);
+                S.Sorba(x);
+                W.Beszur(x);
+            });
+            K.Beir(start, 0);
+            S.Frissit(start);
+            while (!S.Ures)
+            {
+                V u = S.Sorbol();
+                W.Torol(u);
+                g.Szomszedai(u).Bejar(x =>
+                {
+                    if(W.Eleme(x) && g.Suly(u, x) < K.Kiolvas(x))
+                    {
+                        K.Beir(x, g.Suly(u, x));
+                        P.Beir(x, u);
+                        S.Frissit(x);
+                    }
+                });
+            }
+            return P;
+        }
+
+        public static Halmaz<E> Kruskal<V, E>(SulyozottGraf<V, E> g) where E : SulyozottGrafEl<V>, IComparable<E>
+        {
+            Halmaz<E> A = new FaHalmaz<E>();
+            Szotar<V, int> vhalmaz = new HasitoSzotarTulcsordulasiTerulettel<V, int>(g.CsucsokSzama);
+            KupacPrioritasosSor<E> S = new KupacPrioritasosSor<E>(g.ElekSzama, (e1, e2) => e1.Suly < e2.Suly);
+            
+            int i = 0;
+            g.Csucsok.Bejar(x => { vhalmaz.Beir(x, i++); });
+
+            g.Elek.Bejar(e1 =>
+            {
+                S.Sorba(e1);
+            });
+
+            while(!S.Ures)
+            {
+                E e1 = S.Sorbol();
+                V u = e1.Honnan;
+                V v = e1.Hova;
+
+                int hu = vhalmaz.Kiolvas(u);
+                int hv = vhalmaz.Kiolvas(v);
+
+                if(hu != hv)
+                {
+                    A.Beszur(e1);
+
+                    int from = hv;
+                    int to = hu;
+
+                    g.Csucsok.Bejar(x =>
+                    {
+                        if (vhalmaz.Kiolvas(x) == from)
+                        {
+                            vhalmaz.Beir(x, to);
+                        }
+                    });
+                }
+            }
+
+            return A;
         }
     }
 }
